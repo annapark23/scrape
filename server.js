103d319f15b1de71b12a097f034e21ede513b023
Anna Park <annapark23@gmail.com>
2019-01-08 15:01:15 -0600

    consoling out connection credentials

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // mongoose.connect("mongodb://localhost/scrapeHW", { useNewUrlParser: true });
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapeHW";
-
+console.log(MONGODB_URI);
 mongoose.connect(MONGODB_URI);
 
 // Routes
